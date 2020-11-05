c07c4d4be9134616f356331eab27911a1cc60918
Chinmay Pingle <36815409+cpingle36@users.noreply.github.com>
2020-11-05 12:36:57 +0530

    Changes made into app.py from form.py
    Changes made into app.py from form.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, session
+from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL,MySQLdb
+from MySQLdb import escape_string as thwart
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import subprocess
+import re
 
+from flask_session import Session
+
+  
 app = Flask(__name__)
 
-conn = MySQLdb.connect(host="sql12.freemysqlhosting.net",user="sql12366978",password="PlNdNPiBmB",db="sql12366978") 
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+
+#SqlAlchemy Database Configuration With Mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sammy:root@localhost/databast_be'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#for regular mysql
+conn = MySQLdb.connect(host="localhost",user="sammy",password="root",db="databast_be") 
+cursor = conn.cursor()
+
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -12,55 +32,205 @@ def index():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-	# print("hello")
+
 	# print(request.method)
-	
 	if(request.method == "POST"):
 		# print(request.method)
 		attempted_username = request.form.get("username")
 		attempted_password = request.form.get("password")
-		# print(attempted_username)
-		# print(attempted_password)
-		cursor = conn.cursor()
-		now = datetime.now()
-		query1 = "INSERT INTO user (userName,password) VALUES (%s, %s)"
-		val1 = (attempted_username,attempted_password)
-		cursor.execute(query1, val1)
-
-		# query2 = "SELECT userID FROM user WHERE userName = '%s'" % attempted_username
-		# cursor.execute(query2)
-		# records = cursor.fetchall()
-		# print("Total number of rows is: ", cursor.rowcount)
-		# for row in records:
-		# 	userid = row[0]
-		# print("id", userid)
 		
-		print("yeye")
-		query = "INSERT INTO session (loginTime,userID_fk) VALUES (%s, (SELECT userID FROM user WHERE userName=%s))"
+		query = "SELECT password FROM user WHERE userName = '%s'" % attempted_username
+		
+		cursor.execute(query)
+		records = cursor.fetchall()
+		for row in records:
+		 	Pass = row[0]
+		print(Pass)
+		print("attempted", attempted_password)
+		
+		if(Pass == attempted_password):
+			print("matched")
+			now = datetime.now()
+			query1 = "INSERT INTO session (loginTime,userID_fk) VALUES (%s, (SELECT userID FROM user WHERE userName=%s))"
 
-		# query3 = "UPDATE session SET loginTime = %s userID = %s WHERE userID = %s" 
-		val =  (now ,attempted_username)
-		cursor.execute(query,val)
-		print("eee")
-		conn.commit()
-		return redirect(url_for('caseManage'))
-	
+			val1 =  (now ,attempted_username)
+			cursor.execute(query1,val1)
+			conn.commit()
+
+			session['user_name'] = attempted_username					#session saved for USER_NAME	
+
+			return redirect(url_for('caseManage'))
+
 	return render_template("login.html")
 	
 
 @app.route('/caseManage',methods=['POST','GET'])
 def caseManage():
+	if(request.method == "POST"):
+		attempted_caseID = request.form.get("caseID")
+		attempted_investigatorID = request.form.get("investigatorID")
+		attempted_casePassword = request.form.get("casePassword")
+		
+
+		
+		query2 = "SELECT caseID FROM case_table WHERE caseID = '%s'" % attempted_caseID
+		cursor.execute(query2)
+		records = cursor.fetchall()
+		print("Total number of rows is: ", cursor.rowcount)
+
+		if(cursor.rowcount == 1):
+			session['case_id'] = attempted_caseID
+			return redirect(url_for('Overview'))
+
+
+	
 	return render_template("caseManage.html")
 
-@app.route('/newCase')
-def Case():
+@app.route('/newCase',methods=['POST','GET'])
+def newCase():
+	if(request.method == "POST"):
+		attempted_caseID = request.form.get("caseID")
+		attempted_caseName = request.form.get("caseName")
+		attempted_description = request.form.get("description")
+		attempted_investigatorName = request.form.get("investigatorName")
+		attempted_casePassword = request.form.get("casePassword")
+		
+		sequence = session.get('user_name')
+		print("seqqqq" + sequence)
+		session['case_id'] = attempted_caseID	
+		val1 = (attempted_caseID,attempted_caseName,attempted_description,attempted_casePassword)
+		print("val", val1)
+
+		query2 = "SELECT caseID FROM case_table WHERE caseID = '%s'" % attempted_caseID
+		cursor.execute(query2)
+		records = cursor.fetchall()
+		print("Total number of rows is: ", cursor.rowcount)
+
+		if(cursor.rowcount > 0):
+			feedback = "caseID already exists"
+			return render_template("newCase.html", feedback=feedback)
+		else:
+			#INSERT INTO THE CASE_TABLE
+			query1 = "INSERT INTO case_table(caseID,caseName,description,casePassword) VALUES(%s, %s, %s, %s)"
+			cursor.execute(query1, val1)
+		
+			#INSERT INTO (CASE_USER_MAPPING) TABLE
+			cursor.execute('''INSERT INTO case_user_mapping(caseID_fk,userID_fk) VALUES(%s, (SELECT userID FROM user WHERE userName=%s))''', (attempted_caseID,attempted_investigatorName))
+			
+			# now = datetime.now()
+			# #INSERT INTO THE (FINDINGS) TABLE
+			# cursor.execute('''INSERT INTO findings(caseID_fk,Datetime_of_the_Finding) VALUES(%s, %s)''', (attempted_caseID,now))
+		
+		
+		conn.commit()
+
+
 	return render_template("newCase.html")	
 
-@app.route('/overview')
-def over():
-	return render_template("overview.html")	
+@app.route('/', methods=['GET'])
+def index1():
+	return redirect(url_for('fetch'))
 
 
+
+@app.route('/fetch', methods=['GET','POST'])
+def fetch():
+
+	print(request.method)
+	cursor = conn.cursor()
+
+	if(request.method == "GET"or "POST"):
+		
+		#now = datetime.now()
+		query1 = "select distinct vmid from mems";
+		cursor.execute(query1)
+		result = cursor.fetchall()
+		myresult=sorted(result)
+		myresult = str(myresult)
+		myresult= re.sub('[(,)]','',myresult)
+		myresult= myresult[1:-1:2]
+		
+	#	for row in myresult:
+			
+	#		print(row)
+
+	return render_template("fetch.html",myresult=myresult)	
+def index2():
+	return redirect(url_for('fetch'))
+
+
+
+@app.route('/fetch', methods=['GET','POST'])
+def afterfetch():
+	fetch()
+	print(request.method)
+	cursor = conn.cursor()
+
+	if(request.method == "GET"or "POST"):
+		vr = request.form.get("vmid")
+		print(request.method)
+		print(vr)
+
+		cursor.execute("SELECT path FROM mems WHERE VMID= %s", (vr, ))
+
+		my = cursor.fetchall()
+		res=my[13:]
+		for h in my:
+			subprocess.run(["scp", res, "prasad@192.168.43.198:my"])
+	return render_template("fetch.html",res=res)	
+
+#Finding table in database db_name
+db = SQLAlchemy(app)
+class Finding(db.Model):
+	__tablename__ = 'Finding'
+	findingID = db.Column('findingID',db.Integer, primary_key = True, autoincrement=True)
+	user_name = db.Column('user_name',db.Text) 
+	caseID_fk = db.Column('caseID_fk',db.Integer) 
+	Description = db.Column('Description',db.Text)
+	Evidence_Details = db.Column('Evidence_Details',db.Text)
+	# File = db.Column('File',db.LargeBinary)
+	Datetime_of_the_Finding = db.Column('Datetime_of_the_Finding',db.DateTime)
+  
+	def __init__(self, user_name,caseID_fk, Description, Evidence_Details, Datetime_of_the_Finding):
+		self.user_name = user_name
+		self.caseID_fk = caseID_fk
+		self.Description = Description
+		self.Evidence_Details = Evidence_Details
+		self.Datetime_of_the_Finding = Datetime_of_the_Finding
+
+db.create_all()
+
+@app.route('/Overview', methods = ['POST','GET'])
+def Overview():
+	case_id = session.get('case_id')
+	print("caseIDDDD" + case_id)
+	all_data = Finding.query.filter_by(caseID_fk=case_id).all()
+	return render_template("Overview.html", Finding = all_data)
+
+#insert data to mysql database via html forms
+@app.route('/insert', methods = ['POST'])
+def insert():
+	if request.method == 'POST':
+		Description = request.form['Description_of_the_Finding']
+		Evidence_Details = request.form['Evidence_details']
+		#File = request.form('File')
+		Datetime_of_the_Finding = request.form['findingtime']
+		name = session.get('user_name')
+		case_id = session.get('case_id')
+
+		my_data123 = (name, case_id, Description, Evidence_Details, Datetime_of_the_Finding)
+		query123 = "INSERT INTO Finding(user_name,caseID_fk,Description,Evidence_Details,Datetime_of_the_Finding) VALUES(%s, %s, %s, %s, %s)"
+		cursor.execute(query123, my_data123)
+		conn.commit()
+		#my_data = Finding(name, case_id, Description, Evidence_Details, Datetime_of_the_Finding)
+		#db.session.add(my_data)
+		#db.session.commit()
+		return redirect(url_for('Overview'))
+
+@app.route('/eviRepo', methods = ['POST','GET'])
+def eviRepo():
+	return render_template("eviRepo.html")	
+	
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
